3f3883f16c4ffb386d33711c54b089b39a58b6db
Taylor Barnes <tbarnes1@vt.edu>
2018-10-15 17:11:47 -0400

    Fix packages in mac scripts

--- dev/mac_build.sh
+++ dev/mac_build.sh
@@ -1,12 +1,29 @@
 cp Makefile.mac ../src/MAKE/MACHINES/Makefile.mac
 
 cd ../src
+make yes-standard
+make no-gpu
+make no-kim
+make no-kokkos
+make no-latte
+make no-meam
+make no-mscg
+make no-poems
+make no-python
+make no-voronoi
+make no-user-qmmm
+make no-mpiio
+make no-reax
+make yes-user-mdi
+
 make mpi-stubs
-make no-user-mdi
 
 #build mdi
 cd ../lib/mdi
 python Install.py -m gcc
+
+#cd ../reax
+#make -j 32 -f Makefile.gfortran
 
 cd ../../src
 make -j 8 mac
