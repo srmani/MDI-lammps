3fee18347bf3d5d11983ab3cb373b0522322184e
Taylor Barnes <taylor.a.barnes@gmail.com>
2019-04-04 13:25:51 -0400

    Remove obsolete code

--- src/USER-MDI/driver.h
+++ src/USER-MDI/driver.h
@@ -30,35 +30,8 @@ class CommandMDI : protected Pointers {
   virtual ~CommandMDI();
   void command(int, char **);
 
- protected:
-  void send_types(Error *);
-  void send_masses(Error *);
-  void receive_coordinates(Error *);
-  void send_coordinates(Error *);
-  void send_charges(Error *);
-  void send_energy(Error *);
-  void send_forces(Error *);
-  void add_forces(Error *);
-  void receive_forces(Error *);
-  void send_cell(Error *);
-  void md_init(Error *);
-  void timestep(Error *);
-  void optg_init(Error *);
-  int inet, master, ierr;
-  int driver_socket;
-  
-  int nat;
-  bool md_initialized; // has the MD simulation been initialized yet?
-
 private:
-  class Irregular *irregular;
-  class Minimize *minimizer;
   class FixMDI *mdi_fix;
-  int most_recent_init; // which MDI init command was most recently received?
-                        // 0 - none
-                        // 1 - MD
-                        // 2 - OPTG
-
 };
 
 }
@@ -73,28 +46,5 @@ E: Illegal ... command
 Self-explanatory.  Check the input script syntax and compare to the
 documentation for the command.  You can use -echo screen as a
 command-line option when running LAMMPS to see the offending line.
-
-E: Cannot use MDI command without atom IDs
-
-Self-explanatory.
-
-E: MDI command requires consecutive atom IDs
-
-Self-explanatory.
-
-E: Unable to connect to driver
-
-Self-explanatory.
-
-E: Unable to ... driver
-
-Self-explanatory.
-
-E: Unknown command from driver
-
-The driver sent a command that is not supported by the LAMMPS 
-interface.  In some cases this might be because a nonsensical 
-command was sent (i.e. "SCF").  In other cases, the LAMMPS 
-interface might benefit from being expanded.
 
 */
--- src/USER-MDI/fix_driver.h
+++ src/USER-MDI/fix_driver.h
@@ -105,4 +105,27 @@ E: Potential energy ID for fix mdi does not exist
 
 Self-explanatory.
 
+E: Cannot use MDI command without atom IDs
+
+Self-explanatory.
+
+E: MDI command requires consecutive atom IDs
+
+Self-explanatory.
+
+E: Unable to connect to driver
+
+Self-explanatory.
+
+E: Unable to ... driver
+
+Self-explanatory.
+
+E: Unknown command from driver
+
+The driver sent a command that is not supported by the LAMMPS 
+interface.  In some cases this might be because a nonsensical 
+command was sent (i.e. "SCF").  In other cases, the LAMMPS 
+interface might benefit from being expanded.
+
 */
